98968ed970d50686742d72cfcadd4c97f852c9e0
Gavin Gammoh <gammohgb@mail.uc.edu>
2025-05-21 15:10:57 -0400

    Fix database recreated bug on review creation

--- examples/start_review.py
+++ examples/start_review.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from autoclean import Pipeline
 
 # Define paths - modify these to match your system
-EXAMPLE_OUTPUT_DIR = Path("C:/Users/Gam9LG/Documents/Autoclean_resting")  # Where processed data will be stored
-CONFIG_FILE = Path("configs/autoclean_config.yaml")  # Path to config relative to working directory OR absolute path
+EXAMPLE_OUTPUT_DIR = Path("C:/Users/Gam9LG/Documents/Autoclean_testing")  # Where processed data will be stored
+CONFIG_FILE = Path("C:/Users/Gam9LG/Documents/Autoclean_testing/autoclean_config.yaml")  # Path to config relative to working directory OR absolute path
 
 """Example of processing a single EEG file."""
 # Create pipeline instance
