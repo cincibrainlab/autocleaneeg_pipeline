8681b2f7ddb73f57b687ac45fab5d55105bccec2
Gavin Gammoh <gammohgb@mail.uc.edu>
2025-08-07 12:11:13 -0400

    Update functions init
    Former-commit-id: e42e7c2da9c01eeb1d4272d2167233d9940186f3 [formerly 3446d9a9d28984f2b3c1f4d6701c33b0fd35476f]
    Former-commit-id: 7f797ebcecd2e1f32745924afdb8033545786cf0

--- examples/single_file.py
+++ examples/single_file.py
@@ -1,11 +1,14 @@
 from autoclean import Pipeline
-
+from mne.io import read_raw_eeglab
 """Example of processing a single EEG file."""
+
+# raw = read_raw_eeglab("C:/Users/Gam9LG/Documents/Autoclean-EEG/output/Test_Merge_07-22-2025/bids/derivatives/autoclean-v2.1.0/intermediate/04_trim/0199_rest_trim_raw.set", preload=True)
+
 # Create pipeline instance
 pipeline = Pipeline(verbose='INFO')
 
 # Process the file
 pipeline.process_file(
-    file_path="C:/Users/Gam9LG/Documents/DATA/p300/170104_C7D1BL_P300.set",
-    task="p300_grael4k",  # Choose appropriate task
+    file_path="C:/Users/Gam9LG/Documents/DATA/stat_learning/1037_slstructured.raw",
+    task="Statistical_Learning",  # Choose appropriate task
 )
