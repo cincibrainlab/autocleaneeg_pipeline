97c4f1b99d6f593c390462ace27c32db95c45d67
Ernie Pedapati <ernest.pedapati@cchmc.org>
2025-09-18 08:21:31 -0400

    Add ICA custom layout helpers and documentation
    Former-commit-id: f53ec8a96d1f4bcafad8b52e89af47774dd5dc8b

--- src/autoclean/functions/visualization/__init__.py
+++ src/autoclean/functions/visualization/__init__.py
@@ -13,6 +13,12 @@ generate_processing_report : Generate HTML processing report
 create_processing_summary : Create JSON processing summary
 """
 
+from .icvision_layouts import (
+    plot_component_for_classification,
+    plot_components_batch,
+    plot_ica_topographies_overview,
+    save_ica_data,
+)
 from .plotting import plot_ica_components, plot_psd_topography, plot_raw_comparison
 from .reports import create_processing_summary, generate_processing_report
 
@@ -22,4 +28,8 @@ __all__ = [
     "plot_psd_topography",
     "generate_processing_report",
     "create_processing_summary",
+    "plot_component_for_classification",
+    "plot_components_batch",
+    "plot_ica_topographies_overview",
+    "save_ica_data",
 ]
