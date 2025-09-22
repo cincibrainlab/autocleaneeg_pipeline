f7d8ce529b313d4330be9550d982bb8e444ee1d5
drpedapati <ernest.pedapati@cchmc.org>
2025-09-22 16:10:43 -0400

    chore(release): bump version to 2.2.7

--- src/autoclean/__init__.py
+++ src/autoclean/__init__.py
@@ -7,7 +7,7 @@ The package provides both a complete pipeline system (Pipeline) and
 standalone signal processing functions that can be used independently.
 """
 
-__version__ = "2.2.6"
+__version__ = "2.2.7"
 
 
 def __getattr__(name):
