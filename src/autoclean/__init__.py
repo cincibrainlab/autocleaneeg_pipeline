23109fa54bd6b6eccc52870531ca257c03eefc62
Ernie Pedapati <ernest.pedapati@cchmc.org>
2026-03-10 11:23:26 -0400

    Bump package version to 3.0.3

--- src/autoclean/__init__.py
+++ src/autoclean/__init__.py
@@ -7,7 +7,7 @@ The package provides both a complete pipeline system (Pipeline) and
 standalone signal processing functions that can be used independently.
 """
 
-__version__ = "3.0.2"
+__version__ = "3.0.3"
 
 
 def __getattr__(name):
